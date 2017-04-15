8870fe31f5353d828c5cec73618566797c07de53
John Allen Register <chosimba0001@gmail.com>
2017-04-15 13:28:25 -0500

    Moving attack interface to correct folder, and first take on entity interface

---
+++ WpfApplication1/interfaces/IEntity.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzPzRPG.interfaces
+{
+
+    public class Thing { public Thing() { } }
+    public struct Position
+    {
+        public double X;
+        public double Y;
+        public double Z;
+    }
+    public enum EntityState { Standing, Walking, Running, Jumping, Casting, Crouching }
+    public enum EntityDamageState { Safe, Hit, Dead }
+
+
+
+    public class Snail : ILivingEntity
+    {
+        private double X = 0;
+        private double Y = 0;
+        private double Z = 0;
+        private double HP = 0;
+        private double MP = 0;
+        private double CurrentSpeed = 0;
+        private EntityDamageState CurrentDmgState = EntityDamageState.Safe;
+        private EntityState CurrentState = EntityState.Standing;
+
+        public Position GPS { get { return new Position { X = X, Y = Y, Z = Z }; } }
+        public Thing EntityData { get { return new Thing(); } }
+        public double MaxHealth { get { return 100; } }
+        public double MaxResource { get { return 10; } }
+        public string Name { get { return "Snail"; } }
+        public double Speed { get { return CurrentSpeed; } }
+
+        public EntityDamageState getDamageState() {
+            return CurrentDmgState;
+        }
+        public EntityState getState() {
+            return CurrentState;
+        }
+        public double getHealth() { return HP; }
+        public double getResource() { return MP; }
+        public void Spawn() {
+            throw new NotImplementedException();
+        }
+
+        public Snail() { }
+    }
+
+
+
+
+
+
+
+    public class Entity : IEntity {
+        private double X = 0;
+        private double Y = 0;
+        private double Z = 0;
+
+        public Position GPS { get { return new Position { X = X, Y = Y, Z = Z }; } }
+        public Thing EntityData { get { return new Thing(); } }
+        public void Spawn() {
+
+        }
+
+        public Entity(double xPos, double yPos, double zPos)
+        {
+            X = xPos;
+            Y = yPos;
+            Z = zPos;
+        }
+        public Entity(Position initPos)
+        {
+            X = initPos.X;
+            Y = initPos.Y;
+            Z = initPos.Z;
+        }
+    }
+
+    interface ILivingEntity : INamedEntity{
+        double MaxHealth { get; }
+        double MaxResource { get; }
+        double Speed { get; }
+        EntityState getState();
+        EntityDamageState getDamageState();
+        double getHealth();
+        double getResource();
+    }
+
+    interface INamedEntity : IEntity {
+        string Name { get; }
+    }
+
+    interface IEntity
+    {
+        Position GPS { get; }
+        Thing EntityData { get; }
+        void Spawn();
+    }
+}
